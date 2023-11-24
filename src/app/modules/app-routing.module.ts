726519b2bcd1ebe31d31eb8b93947522bc9f2019
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-11-23 23:46:17 -0700

    changes

---
+++ src/app/modules/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { AboutmeComponent } from '../components/aboutme/aboutme.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { SearchComponent } from '../components/search/search.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { FeedbackComponent } from '../components/feedback/feedback.component';
+// import { ExternalLinkComponent } from './external-link.component';
+
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  // { path: 'external', component: ExternalLinkComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'feedback', component: FeedbackComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
