b36e29fad3df009fa413124240fd50e9da977be4
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-11-27 20:52:06 -0700

    matrix effect updated

--- src/app/components/home/matrix-effect/matrix-effect.component.ts
+++ src/app/components/home/matrix-effect/matrix-effect.component.ts
@@ -10,12 +10,12 @@ export class MatrixEffectComponent implements AfterViewInit {
   private chars: string[] = [];
 
   ngAfterViewInit() {
-    this.generateChars();
-    this.createMatrixEffect();
+    this.generateChars(); // Generate an array of Unicode characters
+    this.createMatrixEffect(); // Initialize and start the Matrix code rain effect
   }
 
   private generateChars() {
-
+    // Generate Unicode character arrays for different character sets
     const utf8Chars = this.genUnicode(0x0020, 0x007e);
     const katakana = this.genUnicode(0x30A1, 0x30F6);
     const hiragana = this.genUnicode(0x3041, 0x3096);
@@ -23,11 +23,12 @@ export class MatrixEffectComponent implements AfterViewInit {
     const hindiDevanagari = this.genUnicode(0x0900, 0x097f);
     const chineseHan = this.genUnicode(0x4e00, 0x9fff);
 
-    // concatenated
+    // Concatenate character sets into a single array
     this.chars = utf8Chars.concat(cirilic, hiragana);
   }
 
   private genUnicode(start: number, end: number): string[] {
+    // Generate an array of Unicode characters in the specified range
     const result: string[] = [];
     for (let i = start; i <= end; i++) {
       result.push(String.fromCharCode(i));
@@ -36,6 +37,7 @@ export class MatrixEffectComponent implements AfterViewInit {
   }
 
   private getRandomChar(): string {
+    // Get a random character from the generated character set
     return this.chars[Math.floor(Math.random() * this.chars.length)];
   }
 
@@ -46,14 +48,14 @@ export class MatrixEffectComponent implements AfterViewInit {
     const w = (canvas.width = document.body.offsetWidth);
     const h = (canvas.height = document.body.offsetHeight);
     const cols = Math.floor(w / 20) + 1;
-    const ypos = Array(cols).fill(0);
+    const ypos = Array(cols).fill(0); // Initialize an array to store the vertical position of each column
 
     ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, w, h);
+    ctx.fillRect(0, 0, w, h); // Fill the canvas with a black background
 
     const matrix = () => {
       ctx.fillStyle = '#0001';
-      ctx.fillRect(0, 0, w, h);
+      ctx.fillRect(0, 0, w, h); // Clear the canvas with a slightly transparent black background
 
       ctx.fillStyle = '#0f0';
       ctx.font = '15pt monospace';
@@ -61,12 +63,12 @@ export class MatrixEffectComponent implements AfterViewInit {
       ypos.forEach((y, ind) => {
         const text = this.getRandomChar();
         const x = ind * 20;
-        ctx.fillText(text, x, y);
-        if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-        else ypos[ind] = y + 20;
+        ctx.fillText(text, x, y); // Draw a character at the specified position
+        if (y > 100 + Math.random() * 10000) ypos[ind] = 0; // Reset the position if it exceeds a certain threshold
+        else ypos[ind] = y + 20; // Move the position down for the next frame
       });
     };
 
-    setInterval(matrix.bind(this), 50);
+    setInterval(matrix.bind(this), 50); // Set up an interval to continuously update the Matrix effect
   }
 }
