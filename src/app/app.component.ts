a283b49e09104e144e5674cd7a594c5a51039dcd
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2024-02-03 00:51:36 -0700

    toolbar fixed to a single page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myportfolio';
+
+  @ViewChild('container', { read: ElementRef }) container!: ElementRef; // Use ViewChild for the container
+  components: { [key: string]: ElementRef } = {};
+
+  ngAfterViewInit() {
+    this.components = {
+      // Use querySelector to find elements
+      aboutme: this.container.nativeElement.querySelector('#aboutme'),
+      projects: this.container.nativeElement.querySelector('projects'),
+      resume: this.container.nativeElement.querySelector('#resume'),
+      contact: this.container.nativeElement.querySelector('#contact'),
+    };
+  }
+
+  scrollToComponent(component: string): void {
+    const element = this.components[component]?.nativeElement;
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+  }
 }
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from '../components/home/home.component';
-// import { AboutmeComponent } from '../components/aboutme/aboutme.component';
-// import { ProjectsComponent } from '../components/projects/projects.component';
-// import { ContactComponent } from '../components/contact/contact.component';
-// import { ResumeComponent } from '../components/aboutme/resume/resume.component';
+import { AboutmeComponent } from '../components/aboutme/aboutme.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { ResumeComponent } from '../components/aboutme/resume/resume.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
-  // { path: 'aboutme', component: AboutmeComponent },
-  // { path: 'projects', component: ProjectsComponent },
-  // { path: 'contact', component: ContactComponent },
-  // { path: 'resume', component: ResumeComponent },
-
+  { path: '', component: AboutmeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/aboutme', pathMatch: 'full' },
 ];
 
 @NgModule({
