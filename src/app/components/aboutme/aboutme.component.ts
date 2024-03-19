6de1fb9289be1c0efd45966bf0a2c838435e755b
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2024-03-19 00:45:05 -0600

    styled for linting

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
-  styleUrls: ['./aboutme.component.css']
+  styleUrls: ['./aboutme.component.css'],
 })
-export class AboutmeComponent {
+export class AboutmeComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
 
+  ngOnInit(): void {
+    this.loadExternalStyles('/aboutme.component.css');
+  }
+
+  loadExternalStyles(url: string) {
+    const style = this.renderer.createElement('link');
+    style.rel = 'stylesheet';
+    style.href = url;
+    this.renderer.appendChild(document.head, style);
+  }
 }
