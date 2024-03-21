4244e10f898ef79267e487e0add3bcb68211d0c6
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2024-03-20 21:38:32 -0600

    changes

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
   styleUrls: ['./aboutme.component.css'],
 })
-export class AboutmeComponent implements OnInit {
+export class AboutmeComponent {
   constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
-    this.loadExternalStyles('/aboutme.component.css');
-  }
+  // ngOnInit(): void {
+  //   this.loadExternalStyles('aboutme.component.css');
+  // }
 
   loadExternalStyles(url: string) {
     const style = this.renderer.createElement('link');
